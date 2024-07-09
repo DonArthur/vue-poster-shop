5ce76bcde0dce845cf6669eb93287960ee5225ee
geraldy.miero <geraldy.miero@maucash.id>
2024-07-10 00:38:18 +0700

    update: add fontawesome icon dependency

--- src/components/ShoppingCartItem.vue
+++ src/components/ShoppingCartItem.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
         Shopping Cart
+        <FontAwesomeIcon :icon="faCartShopping" />
     </div>
 </template>
 
 <script setup>
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 </script>
 
 <style scoped>
